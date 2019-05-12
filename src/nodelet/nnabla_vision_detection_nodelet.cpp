28afa50f8122768857e61f17de60d67e97d9403f
Masaya Kataoka <ms.kataoka@gmail.com>
2019-05-12 13:03:29 +0900

    add xml parser

--- src/nodelet/nnabla_vision_detection_nodelet.cpp
+++ src/nodelet/nnabla_vision_detection_nodelet.cpp
@@ -9,11 +9,13 @@
 
 // Headers in STL
 #include <fstream>
+#include <map>
 
 // Headers in Boost
 #include <boost/property_tree/ptree.hpp>
 #include <boost/property_tree/xml_parser.hpp>
 #include <boost/foreach.hpp>
+#include <boost/lexical_cast.hpp>
 
 namespace nnabla_vision_detection
 {
@@ -26,7 +28,7 @@ namespace nnabla_vision_detection
                 it_ = boost::shared_ptr<image_transport::ImageTransport>(new image_transport::ImageTransport(*nh_));
                 pnh_->param<std::string>("image_topic", image_topic_, "image_raw");
                 pnh_->param<std::string>("class_meta_file", class_meta_file_, "");
-                ifs_ = std::ifstream(class_meta_file_);
+                ifs_.open(class_meta_file_);
                 if (ifs_.fail())
                 {
                     NODELET_ERROR_STREAM("failed to read vision_info, file " << class_meta_file_ << " does not exist.");
@@ -38,7 +40,28 @@ namespace nnabla_vision_detection
                 {
                     class_meta_str = class_meta_str+str+"\n";
                 }
+                ifs_.close();
                 pnh_->setParam("class_meta_info", class_meta_str);
+                using namespace boost::property_tree;
+                ptree pt;
+                read_xml(class_meta_file_, pt);
+                BOOST_FOREACH (const ptree::value_type& child, pt.get_child("vision_info"))
+                {
+                    if(child.first == "class")
+                    {
+                        boost::optional<int> id = child.second.get_optional<int>("<xmlattr>.id");
+                        boost::optional<std::string> name = child.second.get_optional<std::string>("<xmlattr>.name");
+                        if(id && name)
+                        {
+                            classes_[*id] = *name;
+                        }
+                        else
+                        {
+                            NODELET_ERROR_STREAM("failed to read xml string, file " << class_meta_file_ << " does not exist.");
+                            std::exit(-1);
+                        }
+                    }
+                }
                 vision_info_pub_ = pnh_->advertise<vision_msgs::VisionInfo>("vision_info",1,true);
                 vision_msgs::VisionInfo vision_info_msg;
                 vision_info_msg.header.stamp = ros::Time::now();
@@ -74,7 +97,7 @@ namespace nnabla_vision_detection
             ros::Publisher vision_info_pub_;
             std::string image_topic_;
             std::string class_meta_file_;
-            std::vector<std::string> classes_;
+            std::map<int,std::string> classes_;
             std::ifstream ifs_;
     };
 }
